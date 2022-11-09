444f97e09a6ce8e7a5afd2917c9f52acaf96fa6a
SandDenis <sanddenis.99@gmail.com>
2022-11-09 13:04:51 +0200

    delete user and articles that belong to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
 
  def show
@@ -44,6 +46,13 @@ class UsersController < ApplicationController
     end
  end
 
+ def destroy
+   @user.destroy
+   session[:user_id] = nil
+   flash[:notice] = "Account and all associated articles deleted"
+   redirect_to root_path
+ end
+
 
  private 
 
@@ -55,4 +64,10 @@ class UsersController < ApplicationController
    @user = User.find(params[:id])
  end
 
+ def require_same_user
+   if current_user != @user
+      flash[:alert] = "you can only edit your own account"
+      redirect_to @user
+   end
+ end
 end
